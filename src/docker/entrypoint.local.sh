0d0d137079ebe6cc9e130e24461934d56248bb97
Francisco Barrento <francisco.barrento@gmail.com>
2023-12-31 19:56:11 +0000

    Added Docker File

---
+++ src/docker/entrypoint.local.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+if [ ! -f vendor/autoload.php ]; then
+  composer install --no-progress --no-interaction
+fi
+
+if [! -f  ".env"]; then
+  echo "Creating env file for env $APP_ENV"
+  cp .env.example .env
+else
+  echo "enf file exists."
+fi
+
+php artisan migrate
+php artisan key:generate
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+
+php-fpm -D
+nginx -g "daemon off;"
